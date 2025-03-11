58821f547b5d55213d9d7b6c796cf9a32b598d57
MohmdAshraf22 <mido.ashraf015@gmail.com>
2025-03-11 23:00:37 +0200

    finish admin dashboard

--- lib/modules/task/data/data_source/remote_data_source.dart
+++ lib/modules/task/data/data_source/remote_data_source.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 import 'package:path/path.dart' as path;
 import 'package:tasks_admin/core/utils/firebase_result_handler.dart';
 import 'package:tasks_admin/modules/task/data/model/task.dart';
@@ -9,13 +8,13 @@ import 'package:tasks_admin/modules/task/data/model/task.dart';
 abstract class TaskDataSource {
   Stream<List<TaskModel>> getTasks();
 
-  Future<Result<bool>> deleteFile(String url);
+  Future<Result<bool>> deleteFile(String url, String taskId);
 
   Future<Result<String>> createTask(TaskModel task);
 
   Future<Result<bool>> updateTask(TaskModel task);
 
-  Future<Result<bool>> deleteTask(String taskId);
+  Future<Result<bool>> deleteTask(TaskModel task);
 
   Future<Result<String>> uploadFile({
     required File file,
@@ -27,43 +26,56 @@ class TaskDataSourceImpl implements TaskDataSource {
   final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
   final FirebaseStorage _storage = FirebaseStorage.instance;
   final String _tasks = 'tasks';
+  final String _dashboardDetails = 'dashboardDetails';
 
   @override
   Stream<List<TaskModel>> getTasks() {
     return _fireStore.collection(_tasks).snapshots().map((snapshot) {
       return snapshot.docs.map((doc) => TaskModel.fromDocument(doc)).toList();
-    }).handleError((error) {
-      debugPrint('Error getting tasks: $error');
-    });
+    }).handleError((error) {});
   }
 
   @override
   Future<Result<String>> createTask(TaskModel task) async {
     try {
+      WriteBatch batch = _fireStore.batch();
       final collection = _fireStore.collection(_tasks);
       final id = collection.doc().id;
-      await collection.doc(id).set(task.toMap(id));
+      DocumentReference dashboardRef =
+          _fireStore.collection(_dashboardDetails).doc(_dashboardDetails);
+      DocumentReference taskRef = collection.doc(id);
+      batch.update(dashboardRef, {'pendingTasks': FieldValue.increment(1)});
+      batch.set(taskRef, task.toMap(id));
+      await batch.commit();
       return Result.success("TaskModel added successfully");
     } on FirebaseException catch (e) {
-      debugPrint('Firebase error creating task status: $e');
       return Result.error(e);
     } on Exception catch (e) {
-      debugPrint('Error creating task status: $e');
       return Result.error(e);
     }
   }
 
   @override
-  Future<Result<bool>> deleteTask(String taskId) async {
+  Future<Result<bool>> deleteTask(TaskModel task) async {
     try {
+      WriteBatch batch = _fireStore.batch();
       final collection = _fireStore.collection(_tasks);
-      await collection.doc(taskId).delete();
+      DocumentReference taskRef = collection.doc(task.id);
+      DocumentReference dashboardRef =
+          _fireStore.collection(_dashboardDetails).doc(_dashboardDetails);
+      taskRef.delete();
+      dashboardRef.update({'pendingTasks': FieldValue.increment(-1)});
+      await batch.commit();
+      if (task.voiceUrl != null) {
+        _deleteFile(task.voiceUrl!);
+      }
+      for (String url in task.imagesUrl) {
+        _deleteFile(url);
+      }
       return Result.success(true);
     } on FirebaseException catch (e) {
-      debugPrint('Firebase error deleting task status: $e');
       return Result.error(e);
     } on Exception catch (e) {
-      debugPrint('Error deleting task status: $e');
       return Result.error(e);
     }
   }
@@ -75,10 +87,8 @@ class TaskDataSourceImpl implements TaskDataSource {
       await collection.doc(task.id).update(task.toMap(task.id));
       return Result.success(true);
     } on FirebaseException catch (e) {
-      debugPrint('Firebase error updating task status: $e');
       return Result.error(e);
     } on Exception catch (e) {
-      debugPrint('Error updating task status: $e');
       return Result.error(e);
     }
   }
@@ -96,86 +106,48 @@ class TaskDataSourceImpl implements TaskDataSource {
       final TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() {});
       if (taskSnapshot.state == TaskState.success) {
         final String downloadUrl = await taskSnapshot.ref.getDownloadURL();
-        print(downloadUrl);
-        return Result.success(downloadUrl);
+                return Result.success(downloadUrl);
       } else {
         return Result.error(Exception());
       }
     } on FirebaseException catch (e) {
-      debugPrint('Firebase Storage error: $e');
       return Result.error(e);
-    } on Exception catch (e) {
-      debugPrint('Error uploading file: $e');
-      return Result.error(Exception());
     }
   }
 
   @override
-  Future<Result<bool>> deleteFile(String url) async {
+  Future<Result<bool>> deleteFile(String url, String taskId) async {
     try {
       final Reference storageReference = _storage.refFromURL(url);
       await storageReference.delete();
+      if (url.contains("images")) {
+        await _fireStore.collection(_tasks).doc(taskId).update({
+          'imagesUrl': FieldValue.arrayRemove([url])
+        });
+      } else {
+        await _fireStore
+            .collection(_tasks)
+            .doc(taskId)
+            .update({'voiceUrl': null});
+      }
       return Result.success(true);
     } on FirebaseException catch (e) {
-      debugPrint('Firebase Storage error: $e');
       return Result.error(e);
     } catch (e) {
-      debugPrint('Error deleting file: $e');
+      return Result.error(Exception());
+    }
+  }
+
+  Future<Result<bool>> _deleteFile(String url) async {
+    try {
+      final Reference storageReference = _storage.refFromURL(url);
+      await storageReference.delete();
+
+      return Result.success(true);
+    } on FirebaseException catch (e) {
+      return Result.error(e);
+    } catch (e) {
       return Result.error(Exception());
     }
   }
 }
-
-// // Example usage in a ViewModel (using ChangeNotifier)
-// class TaskModelViewModel extends ChangeNotifier {
-//   final TaskModelDataSource _taskDataSource;
-//   List<TaskModel> _tasks = [];
-//
-//   List<TaskModel> get tasks => _tasks;
-//   String? _errorMessage;
-//
-//   String? get errorMessage => _errorMessage;
-//
-//   TaskModelViewModel(this._taskDataSource) {
-//     _loadTaskModels();
-//   }
-//
-//   void _loadTaskModels() {
-//     _taskDataSource.getTaskModels().listen((tasks) {
-//       _tasks = tasks;
-//       _errorMessage = null;
-//       notifyListeners();
-//     }, onError: (error) {
-//       _errorMessage = 'Failed to load tasks: $error';
-//       notifyListeners();
-//     });
-//   }
-//
-//   Future<void> acceptTaskModel(String taskId) async {
-//     final result = await _taskDataSource.acceptTaskModel(taskId);
-//     _handleResult(result, 'accept');
-//   }
-//
-//   Future<void> cancelTaskModel(String taskId) async {
-//     final result = await _taskDataSource.cancelTaskModel(taskId);
-//     _handleResult(result, 'cancel');
-//   }
-//
-//   Future<void> markTaskModelAsDone(String taskId) async {
-//     final result = await _taskDataSource.markTaskModelAsDone(taskId);
-//     _handleResult(result, 'mark as done');
-//   }
-//
-//   void _handleResult(Result<bool> result, String action) {
-//     if (result.data == true) {
-//       debugPrint('TaskModel $action successful');
-//       _errorMessage = null;
-//       // Optionally, you might want to refresh the task list here
-//       // _loadTaskModels(); // Or just rely on the stream to update the UI
-//     } else {
-//       _errorMessage = 'Failed to $action task: ${result.errorMessage}';
-//       debugPrint(_errorMessage);
-//     }
-//     notifyListeners();
-//   }
-// }
